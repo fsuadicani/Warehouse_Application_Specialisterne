dcc7ffab445cd57a0554dac5a20157a37b535e0a
Ahmadullah Naibi <ahmadullah@hotmail.dk>
2026-03-11 13:34:15 +0100

    First page prototype ui

--- Frontend/warehouse-storage-app/src/App.jsx
+++ Frontend/warehouse-storage-app/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <h1>Warehouse Storage App</h1>
+      <form action="login-form" method="POST">
+
+      <input type="text" id="username" name="username" placeholder="Enter your username" required />
+      &nbsp;
+      <input type="password" id="password" name="password" placeholder="Enter your password" required />
+
+      <div className="form-actions">
+        <label className="remember">
+          <input type="checkbox" id="remember" name="remember" /> Remember me
+        </label>
+        <a href="#">Forgot password?</a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <button type="submit">Login</button>
+    </form>
+
+    <p>Don’t have an account? <a href="#">Sign up</a></p>
+    </div>
+  );
 }
 
-export default App
+export default App;
