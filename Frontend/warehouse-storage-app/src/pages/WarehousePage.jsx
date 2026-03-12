9bfd56de0e24a0365cbd1143106785b7cb501197
fsuadicani <fsuadicani@yahoo.com>
2026-03-12 18:03:53 +0100

    CitySelector added init

--- Frontend/warehouse-storage-app/src/pages/WarehousePage.jsx
+++ Frontend/warehouse-storage-app/src/pages/WarehousePage.jsx
@@ -1,13 +1,31 @@
 import '../css/warehouse.css';
 
 function WarehousePage() {
+  const warehouses = [
+    { by: 'Odense', postnummer: '5000', gade: 'Flakhaven', husnummer: '2' },
+    { by: 'Kobenhavn', postnummer: '1553', gade: 'Radhuspladsen', husnummer: '1' },
+    { by: 'Aarhus', postnummer: '8000', gade: 'Banegardspladsen', husnummer: '1' },
+  ];
+
+
   return (
     <div className="content">
       <h1>Varehuse</h1>
 
+      <form>
+        <a>
+          Vælg en by:
+        </a>
+        <select className="citySelector">
+          <option value="Odense">Odense</option>
+          <option value="København">København</option>
+          <option value="Århus">Århus</option>
+        </select>
+      </form>
+
       <div className="tablecontainer">
         <table>
-          <tbody>
+          <thead>
             <tr>
               <th>By</th>
               <th>Postnummer</th>
@@ -15,27 +33,20 @@ function WarehousePage() {
               <th>Husnummer</th>
               <th>Actions</th>
             </tr>
-            <tr>
-              <td>Odense</td>
-              <td>5000</td>
-              <td>Flakhaven</td>
-              <td>2</td>
-              <td>Action</td>
-            </tr>
-            <tr>
-              <td>København</td>
-              <td>1553</td>
-              <td>Rådhuspladsen</td>
-              <td>1</td>
-              <td>Action</td>
-            </tr>
-            <tr>
-              <td>Århus</td>
-              <td>8000</td>
-              <td>Banegårdspladsen</td>
-              <td>1</td>
-              <td>Action</td>
-            </tr>
+          </thead>
+          <tbody>
+            {warehouses.map((warehouse) => (
+              <tr key={`${warehouse.by}-${warehouse.postnummer}-${warehouse.gade}-${warehouse.husnummer}`}>
+                <td>{warehouse.by}</td>
+                <td>{warehouse.postnummer}</td>
+                <td>{warehouse.gade}</td>
+                <td>{warehouse.husnummer}</td>
+                <td className="row-actions">
+                  <button type="button">Edit</button>
+                  <button type="button">Delete</button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
