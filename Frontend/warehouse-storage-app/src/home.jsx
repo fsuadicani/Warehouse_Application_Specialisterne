449a0bc11186f23c5c109bf2e646d9f7e8650385
Ahmadullah Naibi <ahmadullah@hotmail.dk>
2026-03-12 11:18:30 +0100

    flex wrap for home css

--- Frontend/warehouse-storage-app/src/home.jsx
+++ Frontend/warehouse-storage-app/src/home.jsx
@@ -1,8 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './css/home.css'
+
 function Home() {
     return (
         <div className="home">
-            <h1>Welcome to the Warehouse Storage App</h1>
-            <p>Manage your warehouse storage efficiently and effectively.</p>
+        <a href="#" className="home-link">Home</a>
+        <a href="#" className="home-link">About</a>
+        <a href="#" className="home-link">Contact</a>
             <div className="home-container">
                 <img src="https://static.vecteezy.com/system/resources/thumbnails/022/935/143/small/large-warehouse-for-storage-of-goods-racks-shelves-goods-background-generative-ai-photo.jpg" alt="Warehouse Storage" />
             </div>
--- Frontend/warehouse-storage-app/src/main.jsx
+++ Frontend/warehouse-storage-app/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import App from './App.jsx'
-import Home from './Home.jsx'
+import Home from 'Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
