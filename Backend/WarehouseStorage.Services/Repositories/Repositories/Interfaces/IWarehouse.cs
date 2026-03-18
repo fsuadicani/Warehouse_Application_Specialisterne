b990ff08e6011e76d7bf79586aae5c99a30f04c3
Ahmadullah Naibi <ahnai17@student.sdu.dk>
2026-03-18 12:46:16 +0000

    Create warehouse repo (#43)
    * warehouse interface created
    
    * warehouse repo implementation complete

---
+++ Backend/WarehouseStorage.Services/Repositories/Repositories/Interfaces/IWarehouse.cs
@@ -0,0 +1,13 @@
+using WarehouseStorage.Domain.Models;
+
+namespace WarehouseStorage.Services.Repositories.Interfaces
+{
+    public interface IWarehouse
+    {
+        Task<Warehouse?> GetById(Guid id);
+        Task<Warehouse[]> GetAll(int skip = 0, int take = 100);
+        Task<Warehouse> Add(Warehouse warehouse);
+        Task Update(Warehouse warehouse);
+        Task Delete(Guid id);
+    }
+}
---
+++ Backend/WarehouseStorage.Services/Repositories/Repositories/WarehouseRepository.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using WarehouseStorage.Domain.Models;
+using WarehouseStorage.Services.Repositories.Interfaces;
+
+public class WarehouseRepository : IWarehouse
+{
+    private readonly WarehouseDbContext _context;
+
+    public WarehouseRepository(WarehouseDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Warehouse?> GetById(Guid id)
+    {
+        return await _context.Warehouses.FindAsync(id);
+    }
+
+    public async Task<Warehouse[]> GetAll(int skip = 0, int take = 100)
+    {
+      return await _context.Warehouses
+            .AsNoTracking()
+            .Skip(skip)
+            .Take(take)
+            .ToArrayAsync();
+    }
+
+    public async Task<Warehouse> Add(Warehouse warehouse)
+    {
+        _context.Warehouses.Add(warehouse);
+        await _context.SaveChangesAsync();
+        return warehouse;
+    }
+
+    public async Task Update(Warehouse warehouse)
+    {
+        if (warehouse.Id == Guid.Empty)
+        {
+            throw new ArgumentException("Warehouse ID cannot be empty for update.");
+        }
+        _context.Warehouses.Update(warehouse);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Delete(Guid id)
+    {
+        var warehouse = await _context.Warehouses.FindAsync(id);
+        if (warehouse != null)
+        {
+            _context.Warehouses.Remove(warehouse);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
